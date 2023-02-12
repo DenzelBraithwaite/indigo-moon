492d634c1aea1e6b1843f714e0c173b0ba6ab4f6
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-02-12 17:16:56 -0500

    created beginning of navar

---
+++ src/lib/UI/navbar.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<style>
+    nav {
+        background-color: #fff;
+        height: 0;
+        padding: 2rem;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .title {
+        font-weight: normal;
+    }
+
+    .nav-options a{
+        text-decoration: none;
+        color: #444;
+        display: inline-block;
+
+        display: flex;
+        /* gap: 1rem; */
+    }
+</style>
+
+<nav>
+    <h1 class="title">Indigo Moon Tarot</h1>
+    <div class="nav-options">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Book Online</a>
+        <a href="#">Insta</a>
+    </div>
+</nav>
