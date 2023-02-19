446ee7e235a19f996364beb8553a9f3c72f927cc
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-02-19 10:19:50 -0500

    moved subbanner into banner and fixed width

--- src/lib/UI/HeroBanner.svelte
+++ src/lib/UI/HeroBanner.svelte
@@ -19,26 +19,93 @@
 
     .content {
         color: #fff;
+        min-width: 350px;
         width: 40%;
         margin: 0 auto;
         text-align: center;
     }
 
     .title {
-        font-size: 5rem;
+        font-size: 3.75rem;
+        line-height: 1.25;
         font-weight: 400;
         text-shadow: 0 2px 10px #0000004b;
     }
 
-    .sub {
-        font-size: 2rem;
+    .subtitle {
+        font-size: 1.25rem;
         font-weight: lighter;
     }
+
+    
+  .sub-banner-title {
+    font-size: 2rem;
+    font-weight: 500;
+    line-height: 1;
+  }
+
+  .sub-banner-subtitle {
+    font-size: 1.25rem;
+    font-weight: lighter;
+    line-height: 1;
+  }
+
+  .sub-banner-text {
+    font-size: 1rem;
+    font-weight: lighter;
+  }
+
+  
+  .sub-banner {
+    position: relative;
+    width: 100%;
+    height: 60vh;
+
+    display: flex;
+  }
+
+  .sub-banner img {
+    width: 50%;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  .sub-banner-text-group {
+    background-color: #727557;
+    color: #fff;
+    width: 50%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .sub-banner-content {
+    min-width: 300px;
+    width: 50%;
+    margin: 0 auto;
+  }
 </style>
 
 <div class="banner">
     <div class="content">
         <h1 class="title">Experience Indigo Moon's Magical Shop and Tarot</h1>
-        <p class="sub">A Spiritual Feeling</p>
+        <p class="subtitle">A Spiritual Feeling</p>
     </div>
 </div>
+<section class="sub-banner">
+    <img src="../sub_hero_banner.webp" alt="necklace swaying over cards">
+    <div class="sub-banner-text-group">
+      <div class="sub-banner-content">
+        <h1 class="sub-banner-title">My Background</h1>
+        <h2 class="sub-banner-subtitle">A Spiritual Guide</h2>
+        <p class="sub-banner-text">Welcome, I’m thrilled the stars have brought you here. 
+          I’m a successful Tarot Card Reader and magical crafter based 
+          in Montreal Quebec - here to guide you through life’s most complex and difficult matters. 
+          Whatever you’re going through, I‘m here to help you find your way forward. 
+          From a young age I realized I was blessed with a gift that enabled me to see and sense things others could not. 
+          Let me do the same for you as I have for so many others since 2015. 
+          Contact me to schedule an appointment or phone call reading.</p>
+      </div>
+    </div>
+  </section>
