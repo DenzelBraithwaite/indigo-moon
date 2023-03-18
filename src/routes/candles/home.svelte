1201046295e708c5e2b14620f65b9f88502e4f65
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-03-18 17:16:16 -0400

    created all candle show pages

---
+++ src/routes/candles/home.svelte
@@ -0,0 +1,256 @@
+<script>
+    import { Router, Route, Link } from 'svelte-routing';
+    import { onMount } from 'svelte';
+  
+    import ProductGrid from '../lib/UI/cards/CardGrid.svelte';
+    import Navbar from '../lib/UI/navbar.svelte';
+    import HeroBanner from '../lib/UI/HeroBanner.svelte';
+    import Testimonials from '../lib/UI/cards/Testimonials.svelte';
+    import Quote from '../lib/UI/Quote.svelte';
+    import Map from '../lib/UI/Map.svelte';
+    import Footer from '../lib/UI/Footer.svelte';
+    import About from '../lib/About.svelte';
+  
+    // Readings Grid
+    let readingsCards = [
+      {
+          'src': "/reading1.webp",
+          'alt': "complete me....",
+          'title': "Give it to me straight",
+          'description': "10 min",
+          'cost': "$20"
+        },
+        {
+          'src': "/reading2.webp",
+          'alt': "complete me....",
+          'title': "Clarity Reading",
+          'description': "20 min",
+          'cost': "$30"
+        },
+        {
+          'src': "/reading3.webp",
+          'alt': "complete me....",
+          'title': "Psychic Reading",
+          'description': "30 min",
+          'cost': "$40"
+        }
+      ];
+      
+    // Candles Grid
+    let candleCards = [
+        {
+          'src': "/candle-blue.webp", // Try after with symbol, no string?
+          'alt': "Blue candle.",
+          'title': "Celestial Blue",
+          'description': "Bergamot - For moon magic or financial manifestation.",
+          'cost': "$8"
+        },
+        {
+          'src': "/candle-orange.webp",
+          'alt': "Orange candle.",
+          'title': "Harvest Spice",
+          'description': "Fall time lovers, allow this scent to fill your room, with the sweet fragrances of warm spices and cinnamon.",
+          'cost': "$8"
+        },
+        {
+          'src': "/candle-red.webp",
+          'alt': "Red candle.",
+          'title': "Red Rose",
+          'description': "Bring love and abundance into your life, set an intention and allow your mind to manifest what you desire.",
+          'cost': "$8"
+        },
+        {
+          'src': "/candle-green.webp",
+          'alt': "Green candle.",
+          'title': "Peppermint & Eucalyptus",
+          'description': "Wake up your senses with this fragrant aroma. Perfect for  a bath or morning alertness.",
+          'cost': "$8"
+        },
+        {
+          'src': "/candle-purple.webp",
+          'alt': "Purple candle.",
+          'title': "Lavender",
+          'description': "Indulge in a little “ you time “. Have the perfect relaxing evening, preparing for a goodnight rest and sweet dreams.",
+          'cost': "$8"
+        },
+        {
+          'src': "/candle-yellow.webp",
+          'alt': "yellow candle.",
+          'title': "Chamomile",
+          'description': "Create a relaxing space and indulge your senses in sweet chamomile.",
+          'cost': "$8"
+        }
+      ];
+  
+
+    let scrollValue = 0;
+    
+    function scrollHandler() {
+      // Update current scroll value
+      scrollValue = window.pageYOffset;
+      
+      // store the current scroll position in the browser history
+      history.pushState({scrollY: scrollValue}, "");
+      }
+
+    onMount(() => {
+        window.scrollTo({
+        top: scrollValue, // set the Y position to 500 pixels
+        left: 0, // set the X position to 0 pixels
+        behavior: 'smooth' // add smooth scrolling behavior
+      });
+    })
+
+  </script>
+
+  <svelte:window on:scroll={scrollHandler} />
+  <body>
+    <!-- DESKTOP -->
+    <div class="desktop">
+      <nav>
+        <Navbar />
+      </nav>
+        <header>
+          <HeroBanner />
+        </header>
+        <main>        
+          <section class="main-container">
+            <ProductGrid gridType='readings' btnText="Book Now" title="Readings" cards={readingsCards} />
+          </section>
+          <section class="main-container">  
+            <Testimonials />
+          </section>
+          <section class="main-container">
+            <ProductGrid gridType='candles' btnText="Buy Now" title="Candles" cards= {candleCards}/>
+          </section>
+          <Quote />
+          <section class="main-container">
+            <About />
+          </section>
+          <section class="main-container">
+            <Map />
+          </section>
+          <!-- <Video
+          title="Who am I ?"
+          poster={videoPoster}
+          src="/vids/intro.mp4">
+        </Video> -->
+        <footer>
+          <Footer />
+        </footer>
+      </main>
+  </div>
+  
+  <!-- MOBILE -->
+    <div class="mobile">
+      <header>
+        <HeroBanner />
+      </header>
+        <main>
+          <section class="m__main-container">
+            <ProductGrid gridType="readings" btnText="Book Now" title="Readings" cards={readingsCards} />
+          </section>
+          <section class="m__main-container">  
+            <Testimonials />
+          </section>
+          <section class="m__main-container">
+            <ProductGrid gridType="candles" btnText="Buy Now" title="Candles" cards= {candleCards}/>
+          </section>
+          <Quote />
+          <section class="m__main-container">
+            <About />
+          </section>
+          <section class="map-section m__main-container">
+            <Map />
+          </section>
+        <footer>
+          <Footer />
+        </footer>
+      </main>
+      <nav>
+        <Navbar />
+      </nav>
+    </div>
+  </body>
+  
+  
+  <style>
+    .mobile {
+      display: none;
+    }
+  
+    body {
+      position: relative;
+      overflow-x: hidden;
+      background: linear-gradient(to bottom, #3b263a 15%, #9f9bbde5, #3b263a);
+    }
+  
+    main {
+      background-color: #fffafef3;
+      width: 90%;
+      margin: 0 auto;
+      transform: translate(0, -100px);
+      border-radius: 1rem;
+      box-shadow: 0 2px 10px #00000025;
+      padding-top: 10rem;
+    }
+  
+    header {
+      position: relative;
+    }
+  
+    section {
+      margin-bottom: 15rem;
+    }
+  
+    section:last-of-type {
+      margin-bottom: 5rem;
+    }
+
+    @media (max-width: 450px) {
+      .desktop {
+        display: none;
+      }
+  
+      .mobile {
+        display: block;
+        margin: 0;
+        padding: 0;
+      }
+  
+      body {
+        position: relative;
+        overflow-x: hidden;
+        background: linear-gradient(to bottom, #3b263a 15%, #9f9bbde5, #3b263a);
+        padding-bottom: 10rem; /* Because of main translate 10rem */
+      }
+  
+      main {
+        position: relative;
+        background-color: #fffafef3;
+        width: 95%;
+        margin: 0 auto;
+        border-radius: 0;
+        box-shadow: 0 2px 10px #00000025;
+        transform: translate(0, 8rem);
+        border-radius: 1rem;
+        padding-top: 0;
+      }
+  
+      header {
+        z-index: 1;
+        position:fixed;
+        top: 0;
+        width: 100%;
+      }
+  
+      nav {
+        position: sticky;
+        bottom: 0; 
+      }
+  
+      section {
+        margin-bottom: 10rem;
+      }
+    }
+  </style>
---
+++ src/routes/candles/redCandle.svelte
@@ -0,0 +1,192 @@
+<script>
+  import { Link } from 'svelte-routing';
+  import Navbar from "../lib/UI/navbar.svelte";
+  import HeroBanner from "../lib/UI/HeroBanner.svelte";
+  import Footer from "../lib/UI/Footer.svelte";
+
+  $: visibility = 'hidden';
+  
+  function mobileMenuBtnHandler() {
+      visibility === 'hidden' ? visibility = 'visible' : visibility = 'hidden';
+      console.log('testr')
+  };
+
+  function mobileModalHandler() {
+      visibility = 'hidden';
+  }
+</script>
+  
+<!-- Destkop -->
+<nav class="desktop-nav">
+  <Navbar />
+</nav>
+
+<!-- Mobile -->
+<nav class="mobile-nav">
+  <HeroBanner />
+</nav>
+
+<div class="product-wrapper">
+  <img class="product-img" src="/candle-red.webp" alt="">
+  <div class="product-wrapper-right">
+    <h1>Red Rose</h1>
+    <h2 class="cost">8$</h2>
+    <p class="description">Bring love and abundance into your life, set an intention and allow your mind to manifest what you desire.</p>
+    <a class="btn__buy btn-purple blue-hover"  href="mailto:tiffany@indigomoontarot.com">Buy Now</a>
+  </div>
+</div>
+<div class="center">
+  <Link on:click={() => {window.history.back()}} class="btn__back" to={'/'}>Back</Link>
+</div>
+<footer>
+  <Footer />
+</footer>
+           
+<style>
+  .mobile-nav {
+    display: none;
+  }
+
+  .center {
+    margin: 0 auto 2rem;
+    text-align: center;
+  }
+
+  .product-wrapper {
+    position: relative;
+    background-color: #ffffff;
+    color: #444;
+    margin: 8rem auto 5rem;
+    height: 600px;
+    width: 1000px;
+    text-align: center;
+    box-shadow: 0px 2px 8px #0000001a;
+    border-radius: 1rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0;
+    padding: 0;
+  }
+
+  .product-img {
+    position: relative;
+    height: 100%;
+    width: 55%;
+    border-radius: 1rem 0 0 1rem;
+  }
+
+  .product-wrapper-right {
+    width: 45%;
+    height: 100%;
+    border-radius: 0 1rem 1rem 0;
+    /* transform: translate(-2rem, 0); */
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .description {
+    width: 70%;
+  }
+
+  .cost {
+    font-size: 3rem;
+  }
+
+  p {
+      padding: 0;
+      margin: 0;
+  }
+
+  .btn__buy:link,
+  .btn__buy:visited {
+    transition: background-color 0.15s ease-in;
+    display: inline-block;
+    text-decoration: none;
+    border: none;
+    color: #fff;
+    width: 70%;
+    background-color: #724f71;
+    text-align: center;
+    font-size: 1.125rem;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+    margin-bottom: 1.25rem;
+    box-shadow: 0 2px 8px #0000001c; /* var(--secondary-purple), transparent*/
+  }
+  
+  .btn__buy:active,
+  .btn__buy:hover {
+    background-color: #95719b;
+    box-shadow: 0 2px 10px #00000013; /* var(--secondary-purple), transparent*/
+  }
+
+  :global(.btn__back:link),
+  :global(.btn__back:visited) {
+    transition: background-color 0.1s ease-in;
+    display: inline-block;
+    text-decoration: none;
+    border: none;
+    color: #fff;
+    width: 200px;
+    margin: 0 auto;
+    text-align: center;
+    background-color: #724f71;
+    text-align: center;
+    font-size: 1.125rem;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+    margin-bottom: 1.25rem;
+    box-shadow: 0 2px 8px #0000001c; /* var(--secondary-purple), transparent*/
+  }
+
+  :global(.btn__back:hover),
+  :global(.btn__back:active) {
+    background-color: #95719b;
+  }
+
+  @media (max-width: 450px) {
+    .mobile-nav {
+      display: block;
+    }
+
+    .desktop-nav {
+      display: none;
+    }
+
+  .product-wrapper {
+    margin: 0 auto;
+    margin-top: 10rem;
+    margin-bottom: 3rem;
+    width: 350px;
+
+    flex-direction: column;
+  }
+
+  .product-img {
+    height: 100%;
+    width: 100%;
+    border-radius: 1rem 1rem 0 0;
+    box-shadow: 0 2px 20px #00000030;
+  }
+
+  .product-wrapper-right {
+    box-shadow: 0 2px 20px #00000030;
+    background-color: #ffffffa1;
+    width: 100%;
+    height: 100%;
+    border-radius: 0 0 1rem 1rem;
+    transform: translate(0, -1rem);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  }
+</style>
